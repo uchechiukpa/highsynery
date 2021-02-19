954a559d93dc3e68d523ae977fe53aac3143e907
Ukpa Uchechi <ukpauchechi1@gmail.com>
2021-02-19 16:09:42 +0100

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,92 @@
-import Autocomplete from './Autocomplete';
+// import Autocomplete from './Autocomplete';
 import usStates from './us-states';
+import Complete from './Complete';
 import './main.css';
+import './complete.css'
 
+
+
+
+
+
+  // const Http = new XMLHttpRequest();
+  // const url = 'https://jsonplaceholder.typicode.com/posts';
+  // Http.open("GET", url);
+  // Http.send();
+
+  // Http.onreadystatechange = (e) => {
+  //   console.log(Http.responseText)
+  // }
 
 // US States
-const data = usStates.map(state => ({
-  text: state.name,
-  value: state.abbreviation
-}));
-new Autocomplete(document.getElementById('state'), {
-  data,
-  onSelect: (stateCode) => {
-    console.log('selected state:', stateCode);
-  },
-});
+// var user;
+var datas = []
+const url = 'https://api.github.com/search/users?q=username&per_page=59'
+
+
+// 'https://jsonplaceholder.typicode.com/posts';
+
+
+// $('.btn').click(function () {
+$.get(url, function (data, status) {
+  // console.log(data)
+  data.items.map(item => {
+    // const abbs = item.login.slice(0,2).toUpperCase()
+    // console.log(item)
+    datas.push(item.login)
+    })
+    // datas.push(data.items.login)
+    // console.log(datas)
+  });
+// });
+// console.log(datas)
+
+
+// console.log(datas)
+const myData = []  
+
+// const data = datas.map(state => ({
+//         myData.push(state.name)
+//           // text: state.name,
+//           // value: state.id
+
+//     }));
+
+const data = datas.map(state => {
+  // console.log(state)
+  myData.push(state.login)
+  // console.log(myData)
+})
+// datas.map(state => ({
+//   text: state.name,
+//   value: state.abbreviation
+
+// }));
+// console.log(data)
+// console.log(datas)
+
+// console.log(usStates)
+// new Autocomplete(document.getElementById('state'), {
+//   data,
+//   onSelect: (stateCode) => {
+//     console.log('selected state:', stateCode);
+
+//   },
+// });
+
+Complete(document.getElementById("myInput"), datas)
+
+
+// const Http = new XMLHttpRequest();
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+// Http.open("GET", url);
+// Http.send();
+
+// Http.onreadystatechange = (e) => {
+//   console.log(Http.responseText)
+// }
+
+
 
 
 // Github Users
